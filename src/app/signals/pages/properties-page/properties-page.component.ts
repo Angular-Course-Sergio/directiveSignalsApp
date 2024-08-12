46acfdc9e781e3dd8230cb7e2191e914d744ed5d
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-12 07:00:06 -0700

    Estructura de pantalla para mutaciones

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
 export class PropertiesPageComponent {
-
+  onFieldUpdated(field: string, value: string) {}
 }
