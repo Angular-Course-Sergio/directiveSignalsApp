8b9f6f975adacf01102f050a584c7974ec293864
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-12 06:46:00 -0700

    Señales - Información del usuario

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces';
 
 @Component({
   templateUrl: './user-info-page.component.html',
-  styleUrl: './user-info-page.component.css'
+  styleUrl: './user-info-page.component.css',
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private usersService = inject(UsersServiceService);
 
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById(id).subscribe((user) => {
+      this.currentUser.set(user);
+    });
+  }
 }
